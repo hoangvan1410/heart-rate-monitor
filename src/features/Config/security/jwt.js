c9a095f8a9ef57a1f13ecff1be59f62df90a8ec8
DESKTOP-D5Q9VK5\Administrator <ppaax1@gmail.com>
2021-05-28 00:23:51 +0700

    decode token

--- src/features/Config/security/jwt.js
+++ src/features/Config/security/jwt.js
@@ -4,6 +4,11 @@ function createToken(userId) {
     return jwt.sign({ _id: userId }, process.env.TOKEN_KEY);
 }
 
+function decodeToken(token) {
+    return jwt.verify(token, process.env.TOKEN_KEY);
+}
+
 module.exports = {
     createToken,
+    decodeToken
 };
