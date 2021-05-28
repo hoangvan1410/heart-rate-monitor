e280380a15e7243ff8f20dfbae2ca5d7cb6a2246
HongCong9991 <lehongcong422@gmail.com>
2021-05-28 17:41:48 +0700

    small fix

--- src/features/Rate/controllers/rate.controller.js
+++ src/features/Rate/controllers/rate.controller.js
@@ -5,8 +5,8 @@ const RateRepository = require("../repository/rate.repository");
 module.exports.index = async (req, res) => {
     const heartRateNumber = req.body.heartRatNumber;
     const label = req.body.label;
-    const createDate = req.body.createDate;
-    const userId = jwt.decodeToken(req.body.token);
+    const createDate = Date.now();
+    const userId = "";
 
     const rate = new Rate({
         heartRateNumber: heartRateNumber,
@@ -14,12 +14,12 @@ module.exports.index = async (req, res) => {
         createDate: createDate,
         userId: userId,
     });
-    RateRepository.addRate(rate);
+    await RateRepository.addRate(rate);
 };
 
 module.exports.postArr = async (req, res) => {
     const rates = req.body.grapValues;
-    const userId = jwt.decodeToken(req.body.token);
+    const userId = "";
     rates.forEach((rate) => {
         const heartRateNumber = rate.heartRateNumber;
         const label = rate.label;
@@ -30,6 +30,6 @@ module.exports.postArr = async (req, res) => {
             createDate: createDate,
             userId: userId,
         });
-        RateRepository.addRate(newRate);
+        await RateRepository.addRate(newRate);
     });
 };
