46e22732c5b1e70a8c6eda1e85bc9c2e23195c09
DESKTOP-D5Q9VK5\Administrator <ppaax1@gmail.com>
2021-05-29 09:42:01 +0700

    reused verifyUser of Hongcong9991

--- src/features/HealthInfo/router/healthInfo.router.js
+++ src/features/HealthInfo/router/healthInfo.router.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/healthInfo.controller.js");
+const verifyUser = require("../../Config/middleware/verifyUser");
 
-router.post("/get", controller.getHealthInfo);
-router.post("/update", controller.updateHealthInfo);
+router.post("/get",verifyUser, controller.getHealthInfo);
+router.post("/update",verifyUser, controller.updateHealthInfo);
 
 module.exports = router;
